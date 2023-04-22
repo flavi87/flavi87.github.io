a63708d6959d916eaaf5c682ddfc32f4545c2662
flavi87 <128279553+flavi87@users.noreply.github.com>
2023-04-22 13:57:41 -0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon');
-const menuDropdown = document.querySelector('.menu-dropdown');
-
-menuIcon.addEventListener('click', () => {
-  menuDropdown.classList.toggle('show');
-});
